f77669b09ad74c1e5d67b2e409ce4324edbc6a23
Juan D. Sevillano <sevi0_6@hotmail.com>
2025-09-08 10:12:29 +0200

    fix: Page suspense added

--- src/contexts/app-context/Page.tsx
+++ src/contexts/app-context/Page.tsx
@@ -1,3 +1,8 @@
+import { Suspense } from 'react';
 import * as styles from './Page.css';
 // TODO: here we can make a layout Page main screen so all Pages goes by the same structure
-export const Page = ({ children }: { title: string; children: React.ReactNode }) => (<div className={styles.pageContainer}>{children}</div>);
+export const Page = ({ children }: { title: string; children: React.ReactNode }) => (
+    <Suspense fallback={<h1>Cargando</h1>}>
+        <div className={styles.pageContainer}>{children}</div>
+    </Suspense>
+);
