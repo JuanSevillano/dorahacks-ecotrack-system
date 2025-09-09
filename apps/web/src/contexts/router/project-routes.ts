3da5ec2550c827c789451cdc61740a9624fae1a3
Juan D. Sevillano <sevi0_6@hotmail.com>
2025-09-10 00:47:21 +0200

    feat: init NFTDetail page as unique route

--- apps/web/src/contexts/router/project-routes.ts
+++ apps/web/src/contexts/router/project-routes.ts
@@ -1,12 +1,12 @@
 import { lazy } from "react";
 import { Paths } from "./types";
-import { NFTDetails } from '../../pages/NFTs'
 
 const ProjectDetail = lazy(() => import('../../pages/projects/ProjectDetail'));
+const NFTDetail = lazy(() => import('../../pages/projects/NFTDetail'));
 
 const routesDefinitions: Record<string, Paths[number]> = {
     projectDetail: { path: 'project/:projectHash', Component: ProjectDetail },
-    projectItemDetail: { path: 'project/:projectHash/:nftHash', Component: NFTDetails }
+    projectItemDetail: { path: 'project/:projectHash/:nftHash', Component: NFTDetail }
 } as const;
 
 export const projectRoutes: Paths = Object.values(routesDefinitions).map(it => ({ ...it }));
